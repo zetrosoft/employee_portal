2e053dc0a1b176a9bb79eb5f817af2124be15c54
Bijak Technology <iswanputera@gmail.com>
2025-12-17 23:20:12 +0700

    fix workflow expenses claim

--- employee_portal/overrides/payment_entry.py
+++ employee_portal/overrides/payment_entry.py
@@ -11,28 +11,6 @@ class PaymentEntryCustom(PaymentEntry):
             valid_doctypes = tuple(valid_doctypes_list)
         return valid_doctypes
 
-    def validate(self):
-        # Cek apakah ini adalah kasus khusus untuk Expense Claim
-        is_expense_claim_ref = self.reference_doctype == "Expense Claim"
-
-        if is_expense_claim_ref:
-            # Simpan nilai asli untuk dikembalikan nanti
-            original_ref_doctype = self.reference_doctype
-            original_ref_name = self.reference_name
-            
-            # Hapus sementara referensi agar validasi inti lolos
-            self.reference_doctype = None
-            self.reference_name = None
-
-        try:
-            # Selalu jalankan validasi inti dari parent class
-            super(PaymentEntryCustom, self).validate()
-        finally:
-            # Jika ini adalah kasus khusus, kembalikan nilai asli setelah validasi selesai
-            if is_expense_claim_ref:
-                self.reference_doctype = original_ref_doctype
-                self.reference_name = original_ref_name
-
     def on_submit(self):
         # Jalankan dulu on_submit standar
         super(PaymentEntryCustom, self).on_submit()
@@ -40,7 +18,6 @@ class PaymentEntryCustom(PaymentEntry):
         # LOGIKA UNTUK UPDATE STATUS EXPENSE CLAIM
         if self.reference_doctype == "Expense Claim" and self.reference_name:
             try:
-                # Dapatkan dokumen Expense Claim yang dirujuk
                 expense_claim_doc = frappe.get_doc("Expense Claim", self.reference_name)
                 
                 if hasattr(expense_claim_doc, "update_paid_status"):
